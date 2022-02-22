663261633e45098e3ae957483dcb6f3551be097e
Robby Hill <robbyhill20@yahoo.com>
2022-02-22 08:39:36 -0800

    more models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -7,6 +7,16 @@ class Category extends Model {}
 Category.init(
   {
     // define columns
+    id:{
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    catName:{
+      type:DataTypes.STRING,
+      allowNull: false,
+    }
   },
   {
     sequelize,
